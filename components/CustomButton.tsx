e536fd219bb3f8b22eea94456cc93600faa55563
Sunilkumar35 <rkt.sunilkumar97@gmail.com>
2024-04-23 03:12:15 -0700

    first commit

---
+++ components/CustomButton.tsx
@@ -0,0 +1,16 @@
+import { CustomButtonProps } from "@/types";
+
+const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => (
+  <button
+    disabled={false}
+    type="button"
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}
+  >
+    <span className="flex-1">
+      {title}
+    </span>
+  </button>
+);
+
+export default CustomButton;
---
+++ types/index.jsx
@@ -0,0 +1,20 @@
+import { MouseEventHandler } from "react";
+
+
+
+
+export interface HomeProps {
+  searchParams: FilterProps;
+}
+
+
+
+export interface CustomButtonProps {
+  isDisabled?: boolean;
+  btnType?: "button" | "submit";
+  containerStyles?: string;
+  textStyles?: string;
+  title: string;
+  rightIcon?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
