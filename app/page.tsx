911fcf02b23857c127d8d3df2596e1b28d6b30d3
Sunilkumar35 <rkt.sunilkumar97@gmail.com>
2024-04-25 02:30:40 -0700

    sreachbar added

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,18 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
     <Hero/>
+    <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home_text-container">
+            <h1> Car catalogue</h1> 
+            <p> Explore the car yoou might like</p>
+        </div>
+        <div className="home_filters">
+          {/* <SearchBar/> */}
+        </div>
+        <div className="home__filter-container">
+          {/* <CustomFilter /> */}
+        </div>
+    </div>
     </main>
   );
 }
